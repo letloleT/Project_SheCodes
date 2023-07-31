dd1f8b97bfefb907a8e60d1c65ebccef40ce8271
letloleT <letloletshepang@gmail.com>
2023-07-31 22:52:44 +0200

    She Codes Plus Homework

--- weatherApp/src/apps.js
+++ weatherApp/src/apps.js
@@ -31,7 +31,7 @@ function formatDate(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  // console.log(response.data.daily);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
@@ -43,16 +43,16 @@ function displayForecast(response) {
         forecastHTML +
         `
     <div class="col tempCheck">
-      <div>${formatDate(forecastDay.dt)}</div>
-      <img src="https://openweathermap.org/img/wn/${
-        forecastDay.weather[0].icon
-      }@2x.png" class="forecastImg" alt="" />
+      <div>${formatDate(forecastDay.time)}</div>
+      <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+        forecastDay.condition.icon
+      }.png" class="forecastImg" alt="" />
       <div class="temperature">
         <span id="forecast-max-temperature"><strong>${Math.round(
-          forecastDay.temp.max
+          forecastDay.temperature.maximum
         )}°</strong></span>
         <span id="forecast-min-temperature">${Math.round(
-          forecastDay.temp.min
+          forecastDay.temperature.minimum
         )}°</span>
       </div>
     </div>`;
@@ -62,26 +62,25 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
 function getForecast(coordinates) {
-  // console.log(coordinates);
-  let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  // console.log(apiURL);
+  let apiKey = "ta8of46b0a3a9f030b9f4d2457bdb911";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+
   axios.get(apiURL).then(displayForecast);
 }
+
 function showTemperature(response) {
   console.log(response);
-  celsiusTemperature = response.data.main.temp;
-  // let temperature = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.temperature.current;
 
-  let humidity = response.data.main.humidity;
+  let humidity = response.data.temperature.humidity;
   let windSpeed = Math.round(response.data.wind.speed);
-  let description = response.data.weather[0].description;
+  let description = response.data.condition.description;
   let dateElement = document.querySelector("#date");
-  let mainIcon = response.data.weather[0].icon;
-
+  let mainIcon = response.data.condition.icon;
   let cityElement = document.querySelector("#cityName");
-  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.city;
   let temperatureElement = document.querySelector("#theTemperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   let humidityElement = document.querySelector("#theHumidity");
@@ -90,17 +89,18 @@ function showTemperature(response) {
   windSpeedElement.innerHTML = windSpeed;
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = description;
-  dateElement.innerHTML = setDate(response.data.dt * 1000);
+  dateElement.innerHTML = setDate(response.data.time * 1000);
   let mainIconElement = document.querySelector("#mainWeatherIcon");
   mainIconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${mainIcon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${mainIcon}.png`
   );
-  getForecast(response.data.coord);
+  getForecast(response.data.coordinates);
 }
+
 function search(city) {
-  let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "ta8of46b0a3a9f030b9f4d2457bdb911";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiURL).then(showTemperature);
 }
